f47e5d7aa8eb39be6d1feb15fe4c57a5397d343f
Ray Arvin Rimorin <rayarvin@gmail.com>
2012-05-19 23:11:34 +0800

    Initial Commit

---
+++ LyricView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  LyricView
+//
+//  Created by Arvin Rimorin on 5/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ LyricView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LyricView
+//
+//  Created by Arvin Rimorin on 5/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
